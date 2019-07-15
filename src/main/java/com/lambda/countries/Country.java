0412537b724b1279ca7045be8b335c0c08e42286
Firealem Erko <firealemg@yahoo.com>
2019-07-15 15:28:50 -0400

    country class complete

--- src/main/java/com/lambda/countries/Country.java
+++ src/main/java/com/lambda/countries/Country.java
@@ -19,4 +19,49 @@ public class Country
         this.landmassSize = landmassSize;
         this.medianAge = medianAge;
     }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public String getCountryName()
+    {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName)
+    {
+        this.countryName = countryName;
+    }
+
+    public long getPopulation()
+    {
+        return population;
+    }
+
+    public void setPopulation(long population)
+    {
+        this.population = population;
+    }
+
+    public long getLandmassSize()
+    {
+        return landmassSize;
+    }
+
+    public void setLandmassSize(long landmassSize)
+    {
+        this.landmassSize = landmassSize;
+    }
+
+    public int getMedianAge()
+    {
+        return medianAge;
+    }
+
+    public void setMedianAge(int medianAge)
+    {
+        this.medianAge = medianAge;
+    }
 }
